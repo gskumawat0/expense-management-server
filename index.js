0747c2b16824ccc6c69b2ac775a6306cc3252834
Gouri Shankar Kumawat <gskumawat555@gmail.com>
2019-08-03 00:36:52 +0530

    bug fixes

--- index.js
+++ index.js
@@ -14,12 +14,13 @@ mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 
 //log mongoose queries    
 process.env.NODE_ENV === 'development' && mongoose.set('debug', true);
-app.use(bodyparser.json({limit: '5mb', extended: false}));    
-app.use(bodyparser.urlencoded({limit: '5mb', extended: false})) 
+app.use(bodyparser.json({limit: '5mb', extended: true}));    
+app.use(bodyparser.urlencoded({limit: '5mb', extended: true})) 
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', process.env.CLIENT_API_BASE_URL);
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
     next();
 });
 
